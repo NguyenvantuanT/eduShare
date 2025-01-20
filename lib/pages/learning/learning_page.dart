b82e124e6d0cf25b12e865ed10f94d310775ba2a
Tuan Nguyen <tuannguyen@gmail.com>
2025-01-20 18:30:13 +0700

    home

--- lib/pages/learning/learning_page.dart
+++ lib/pages/learning/learning_page.dart
@@ -1,9 +1,11 @@
 import 'package:chat_app/models/course_model.dart';
-import 'package:chat_app/pages/learning/widget/course_learning_item.dart';
+import 'package:chat_app/pages/course_detail/course_detail_page.dart';
+import 'package:chat_app/pages/learning/widget/learning_item.dart';
 import 'package:chat_app/resource/themes/app_colors.dart';
 import 'package:chat_app/resource/themes/app_style.dart';
 import 'package:chat_app/services/local/shared_prefs.dart';
 import 'package:chat_app/services/remote/course_services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class LearningPage extends StatefulWidget {
@@ -16,33 +18,38 @@ class LearningPage extends StatefulWidget {
 class _LearningPageState extends State<LearningPage> {
   CourseServices courseServices = CourseServices();
   List<CourseModel> learningList = [];
-  bool isLoading = false;
 
-  @override
-  void initState() {
-    super.initState();
-    getLearnings();
-  }
-
-  void getLearnings() async {
-    setState(() => isLoading = true);
-    courseServices
-        .getLearnings(SharedPrefs.user?.learnings ?? [])
-        .then((values) {
-      learningList = values ?? [];
-      setState(() {});
-    }).whenComplete(() => setState(() => isLoading = false));
-  }
+  final Stream<QuerySnapshot> courseStream = FirebaseFirestore.instance
+      .collection('courses')
+      .orderBy('id', descending: true)
+      .snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.bgColor,
-      body: isLoading
-          ? const Center(
-              child: CircularProgressIndicator(color: AppColor.blue),
-            )
-          : ListView(
+      body: StreamBuilder(
+          stream: courseStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(color: AppColor.blue),
+              );
+            }
+            learningList = (snapshot.data?.docs
+                        .map((e) => CourseModel.fromJson(
+                            e.data() as Map<String, dynamic>)
+                          ..docId = e.id)
+                        .toList() ??
+                    [])
+                .where((e) =>
+                    (e.learnings ?? []).contains(SharedPrefs.user?.email))
+                .toList();
+
+            return ListView(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               children: [
                 Text(
@@ -55,14 +62,23 @@ class _LearningPageState extends State<LearningPage> {
                   physics: const NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
                   itemCount: learningList.length,
-                  separatorBuilder: (_, __) => const SizedBox(),
+                  separatorBuilder: (_, __) => const SizedBox(height: 20.0),
                   itemBuilder: (context, idx) {
                     final course = learningList[idx];
-                    return CourseLearningItem(course);
+                    return LearningItem(
+                      course,
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              CourseDetailPage(course.docId ?? ""),
+                        ),
+                      ),
+                    );
                   },
                 )
               ],
-            ),
+            );
+          }),
     );
   }
 }
